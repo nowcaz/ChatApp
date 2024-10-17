a6099e1c058f93d0eb2bc601de05a97d1c9a5dcf
Opilane TTHK <opilane@tthk.ee>
2024-10-17 11:28:45 +0300

    muudatused

---
+++ ChatApp-main/ChatApp-main/tunnitoo1-main/ChatApp/ChatConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using ChatClient.Net;
+using ChatConsole;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+
+string name;
+Console.WriteLine("Name: ");
+name = Console.ReadLine();
+Server server = new Server();
+server.ConnectToServer(name);
+server.SendMessageToServer("aaaa");
